629edc5949d64970880aac4f413bfb18640a673a
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-06-11 17:08:26 +0100

    Init commit

--- GestionClientesGimnasio/src/gestionclientesgimnasio/Cliente.java
+++
@@ -1,5 +0,0 @@
-package gestionclientesgimnasio;
-
-public class Cliente {
-
-}
--- GestionClientesGimnasio/src/gestionclientesgimnasio/Gimnasio.java
+++
@@ -1,9 +0,0 @@
-package gestionclientesgimnasio;
-
-public class Gimnasio {
-
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-
-}
